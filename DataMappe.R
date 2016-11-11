3eb54e698adec860f8c257cbfec839366a1a9276
Jack Williams <jw2249a@users.noreply.github.com>
2016-11-11 01:02:43 -0500

    Add files via upload

---
+++ DataMappe.R
@@ -0,0 +1,4 @@
+
+# Make a Cool Map
+# Download District Data From NCES at http://data.deptofed.opendata.arcgis.com/datasets/1e1426f35aef40c48ee8f20e0b9e5bac_0
+# Input SHP file into Mapbox Studio Classic
